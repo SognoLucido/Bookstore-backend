c8bd24ed06e900d552d120afd5b5854d51d7a96b
Francesco Barbano <francesco.barbano96@gmail.com>
2024-08-02 09:49:28 +0200

    base tokenblock

---
+++ Bookstore-backend/AuthTokenblock.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Bookstore_backend
+{
+    public class AuthTokenblock : IAuthorizationHandler, IAuthorizationRequirement
+    {
+        public Task HandleAsync(AuthorizationHandlerContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
